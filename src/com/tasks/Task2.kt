23cd194f98b9131a2b3d5d27b5df5ef773b6c0ed
Olaf Kulczycki <Olaf.Kulczycki@gmail.com>
2024-12-10 13:16:11 +0100

    finished Day 2 of advent of coding.

--- src/com/tasks/Task2.kt
+++ src/com/tasks/Task2.kt
@@ -4,8 +4,10 @@ import java.io.File
 class Task2(private val filePath: String = "src/resources/input_task2") {
 
     // variables
-    private val checkedResult = checkDifferences(readInputTask2())
+    private val result = readInputTask2()
+    private val checkedResult = checkDifferences(result)
     private val countedSaves = countSaves(checkedResult)
+    private val dampenedResult = problemDampener(result, checkedResult)
 
     // functions
     private fun readInputTask2(): List<List<Int>> {
@@ -41,11 +43,46 @@ class Task2(private val filePath: String = "src/resources/input_task2") {
         return checkedSaves.count { it == "safe" }
     }
 
+    private fun problemDampener(result: List<List<Int>>, checkedResult: List<String>): List<String> {
+        val newCheckedResult = checkedResult.toMutableList()
+
+        for (lineIndex in result.indices) {
+            if (checkedResult[lineIndex] == "unsafe") {
+                val line = result[lineIndex]
+                for (i in line.indices) {
+                    val modifiedLine = line.toMutableList().apply { removeAt(i) }
+                    if (isLineSafe(modifiedLine)) {
+                        newCheckedResult[lineIndex] = "safe"
+                        break
+                    }
+                }
+            }
+        }
+
+        return newCheckedResult
+    }
+
+    private fun isLineSafe(line: List<Int>): Boolean {
+        if (line.size < 2) return false
+        val isIncreasing = line[1] > line[0]
+
+        for (i in 0..<line.size - 1) {
+            val difference = kotlin.math.abs(line[i] - line[i + 1])
+            if (difference !in 1..3 || (line[i + 1] > line[i]) != isIncreasing) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     // on init
     init {
         println("The input of task 2 is: $filePath")
         println("input read correctly, starting to count the safe routes...")
         println("The amount of safe routes are: $countedSaves")
+        println("Using the Problem Dampener to check the unsafe reports")
+        println("New amount of safe reports are ${countSaves(dampenedResult)}")
     }
 }
 
