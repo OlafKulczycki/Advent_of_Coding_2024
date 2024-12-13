268bc2354e943dccbca50a5efc3d11f4225a2608
Olaf Kulczycki <Olaf Kulczycki@users.noreply.github.com>
2024-12-13 22:37:10 +0100

    Completed Day 3 of advent of coding. Added refined_input_task3 to resources.

--- src/com/tasks/Task3.kt
+++ src/com/tasks/Task3.kt
@@ -5,27 +5,54 @@ private class Task3 {
 
     //variables
     val rawInput = readFileToList("src/resources/input_task3")
-    val sumMulCalls = extractMulCalls(rawInput).sum()
+    val refinedInput = filterInstructions(rawInput.joinToString(" "))
+    val sumMulCalls = extractMulCalls("src/resources/refined_input_task3").sum()
 
     //functions
-    fun extractMulCalls(inputs: List<String>): List<Int> {
+    fun extractMulCalls(filePath: String): List<Int> {
         val regex = """mul\((\d+),\s*(\d+)\)""".toRegex()
-        return inputs.flatMap { input ->
-            regex.findAll(input).map { matchResult ->
-                val (a, b) = matchResult.destructured
-                a.toInt() * b.toInt()
-            }.toList()
+        val result = mutableListOf<Int>()
+        var addCalls = true // mul instructions are enabled at the beginning
+
+        val inputs = File(filePath).readLines()
+
+        for (input in inputs) {
+            if (input.contains("do()")) {
+                addCalls = true
+            } else if (input.contains("don't()")) {
+                addCalls = false
+            }
+
+            if (addCalls) {
+                regex.findAll(input).forEach { matchResult ->
+                    val (a, b) = matchResult.destructured
+                    result.add(a.toInt() * b.toInt())
+                }
+            }
         }
+        return result
     }
 
     fun readFileToList(filePath: String): List<String> {
         return File(filePath).readLines()
     }
 
+    fun filterInstructions(input: String): List<String> {
+        val regex = """(do\(\)|don't\(\)|mul\(\d+,\d+\))""".toRegex()
+        return regex.findAll(input).map { it.value }.toList()
+    }
+
     //init block
     init {
         println("Reading the input file src/resources/input_task3")
-        println("File read successfully, the result is: $sumMulCalls")
+        println("File read successfully and filtered. Saving the refined input to src/resources/refined_input_task3")
+        println("The result is: $sumMulCalls")
+        saveRefinedInputToFile("src/resources/refined_input_task3", refinedInput)
+    }
+
+    fun saveRefinedInputToFile(filePath: String, content: List<String>): List<String> {
+        File(filePath).writeText(content.joinToString("\n"))
+        return content
     }
 }
 
